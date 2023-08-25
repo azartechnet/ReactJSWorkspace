45aba19c6e36b26fbc297dbff6cb28ac0de7d1aa
MohamedAzarudeen <azartechnet@gmail.com>
2023-08-25 10:48:13 +0530

    25-08-2023

--- fullstackb5/src/index.js
+++ fullstackb5/src/index.js
@@ -1,20 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// class Sample extends React.Component
-// {
-//   render()
-//   {
-//     return <h1>Welcome</h1>;
-//   }
-// }
-
-// const container = document.getElementById('root');
-// const root = ReactDOM.createRoot(container);
-// root.render(<Sample />);
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Sample from './Sample.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Sample />);
+class Demo extends React.Component
+{
+    render(){
+        return <p>Welcome to the class Component</p>
+    }
+}
+
+const c1 = document.getElementById('root');
+const root = ReactDOM.createRoot(c1);
+root.render(<Demo />);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import Sample from './Sample.js';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<Sample />);
---
+++ fullstackb6/src/Sample.js
@@ -0,0 +1,5 @@
+function Sample()
+{
+    return <h1>Welcome to the Sample.js</h1>
+}
+export default Sample;
