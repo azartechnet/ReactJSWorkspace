3febdbafe58971091dc4f6e9e562ce1ea7aa51fd
MohamedAzarudeen <azartechnet@gmail.com>
2023-09-06 22:41:46 +0530

    06-09-23

--- projectp1/src/index.js
+++ projectp1/src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import  ReactDOM  from 'react-dom';
+import  './index.css'
+/*function Sample()
+{
+  return<h1>Welcome to the React Application</h1>
+}
+const c1=document.getElementById("root");
+const r1=ReactDOM.createRoot(c1);
+r1.render(<Sample/>);*/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Basic Rendering Concepts
+/*const App=()=>{
+  return(
+    <div>
+      <h1>Welcome to the Application</h1>
+      <p>This is the Paragraphs</p>
+    </div>
+  );
+};
+*/
+const App=()=>{
+  return(
+    <div>
+      <h1>LoginForm</h1>
+      <form>
+        UserName:<input type='text'/>
+        Password:<input type='password'/>
+        <input type='submit' value='Login'/>
+      </form>
+    </div>
+  );
+};
+ReactDOM.render(<App/>,document.getElementById("root"));
