5163045884bbed8a94782c5811a1b5b91ddb8e52
azartechnet <azartechnet@gmail.com>
2023-10-05 10:31:17 +0530

    05-1-23

--- mernstackp1/src/index.js
+++ mernstackp1/src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import React from "react";
+// import ReactDOM from "react-dom/client"
+/*
+function Sample()
+{
+  return<h1>Welcome to the Function</h1>
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const c1=document.getElementById('root');
+const r1=ReactDOM.createRoot(c1);
+r1.render(<Sample/>)*/
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//Basic Rendering
+import React from "react";
+import ReactDOM from "react-dom";
+const  App=()=>{
+  return(
+
+    <div>
+      <h1>Heading-1</h1>
+      <p>This is Para...</p>
+    </div>
+  );
+};
+ReactDOM.render(<App/>,document.getElementById('root'))
