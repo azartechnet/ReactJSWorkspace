8b829923430fefff28dc2876e655e2560a18bd72
azartechnet <azartechnet@gmail.com>
2023-10-06 12:15:39 +0530

    updated

--- mernstackp1/src/index.js
+++ mernstackp1/src/index.js
@@ -1,17 +1,29 @@
-// import React from "react";
-// import ReactDOM from "react-dom/client"
-/*
-function Sample()
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+/*function Sample()
 {
-  return<h1>Welcome to the Function</h1>
+  return(
+    <div>
+<h1>Welcome to the Function</h1>
+    <h2>Welcome to heading</h2>
+    <form>
+      UserName:<input type="text"/>
+      Password:<input type="text"/>
+      <input type="submit" value="ClickHere"/>
+    </form>
+    </div>
+    
+  )
+  
 }
 
 const c1=document.getElementById('root');
 const r1=ReactDOM.createRoot(c1);
-r1.render(<Sample/>)*/
-
+r1.render(<Sample/>)
+*/
 //Basic Rendering
-import React from "react";
+/*import React from "react";
 import ReactDOM from "react-dom";
 const  App=()=>{
   return(
@@ -22,4 +34,48 @@ const  App=()=>{
     </div>
   );
 };
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App/>,document.getElementById('root'))*/
+//Dynamic Content
+/*import React from "react";
+import ReactDOM from "react-dom"
+const App=()=>{
+  const username='azar';
+  return(
+    <div>
+      <h1>Welcome</h1>
+      <p>{new Date().toLocaleDateString()}</p>
+      <h2>{username}</h2>
+    </div>
+  )
+}
+ReactDOM.render(<App/>,document.getElementById('root'));*/
+
+//JSX Concepts
+// const my1=<h1>Welcome to JSX</h1>
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(my1);
+
+//Expression in JSX
+// const my1=<h1>React {5+5}</h1>
+// const r1=ReactDOM.createRoot(document.getElementById('root'))
+// r1.render(my1)
+
+//Inserting the list
+/*const my1=(
+  <ul>
+    <li>MCA</li>
+    <li>MBA</li>
+    <li>Bsc</li>
+  </ul>
+);
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(my1)*/
+
+//fragement tag
+const my1=(
+  <>
+  <h1>Welcome to the First App</h1>
+  </>
+)
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(my1)
