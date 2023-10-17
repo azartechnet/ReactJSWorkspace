ccfcde3739cb61e5a86a9a8e202e17fee209ece6
azartechnet <azartechnet@gmail.com>
2023-10-17 07:44:26 +0530

    updated

--- mernstackeve/src/index.js
+++ mernstackeve/src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom/client'
+function Sample()
+{
+  return<h1>Welcome to the function</h1>
+}
+const c1=document.getElementById('root')
+const r=ReactDOM.createRoot(c1)
+r.render(<Sample/>);
