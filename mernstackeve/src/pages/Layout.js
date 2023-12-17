7bb1cd041e6d4275c75835c45b98d16e81f703b6
azartechnet <azartechnet@gmail.com>
2023-12-17 11:15:50 +0530

    updated

--- mernstackeve/src/pages/Layout.js
+++ mernstackeve/src/pages/Layout.js
@@ -1,22 +1,26 @@
 import { Outlet,Link } from "react-router-dom";
 const Layout=()=>{
     return(
-        <>
-            <nav>
-              <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/blogs">Blogs</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-              </ul>    
-            </nav> 
-            <Outlet/>  
-        </>
+        
+      
+      <div class="navbar">
+        <a class="navbar-brand" href="#">Title</a>
+        <ul class="nav navbar-nav">
+          <li class="active">
+          <Link to="/">Home</Link>
+          </li>
+          <li>
+          <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+          <Link to="/contact">Contact</Link>
+          </li>
+          
+        </ul>
+        <Outlet/>
+      </div>
+     
+      
     )
 };
 export default Layout;
