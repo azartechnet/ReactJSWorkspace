c24c4169a8ce558fc996adb63ae8b53e2ae3159f
azartechnet <azartechnet@gmail.com>
2024-02-01 10:27:05 +0530

    updated

--- jessilprj/src/pages/Layout.js
+++ jessilprj/src/pages/Layout.js
@@ -1,15 +1,29 @@
 import { Outlet,Link } from "react-router-dom";
+import "./layout.css";
 const Layout=()=>{
     return(
         <>
-        <nav>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/login">Login</Link></li>
-                <li><Link to="/register">Register</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+         
+         
+         <div class="navbar nav1">
+            <a class="navbar-brand" href="#">E-KART</a>
+            <ul class="nav navbar-nav">
+                <li class="active">
+                <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/login">Login</Link>
+                </li>
+                <li>
+                <Link to="/register">Register</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
             </ul>
-        </nav>
+         </div>
+         
+
         <Outlet/>
         </>
     )
