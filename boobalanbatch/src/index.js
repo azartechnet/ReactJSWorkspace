6adb74a44ae604f352265cfaf5510b4aaf2418da
azartechnet <azartechnet@gmail.com>
2024-02-23 13:18:49 +0530

    updated

--- boobalanbatch/src/index.js
+++ boobalanbatch/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-const  App = () => {
+/*const  App = () => {
    return (
       <div>
         <h1>Welcome to the First Program</h1>
@@ -12,6 +12,36 @@ const  App = () => {
         </form>
       </div>
    )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+
+//Basic Rendering
+
+/*const App=()=>{
+  return(
+    <div>
+    <h1>Welcome to Baics Rendering</h1>
+    </div>
+  )
+}
+
+const r1=ReactDOM.createRoot(document.getElementById('root'))
+r1.render(<App/>)*/
+
+//Dynamic Rendering
+
+const App=()=>{
+  const username="mohamed";
+  return(
+    <>
+      <h1>Hello{username}</h1>
+      <h2>I am {username}'s Profile Page</h2>
+       <p>{new Date().toLocaleDateString()}</p>
+    </>
+  )
 }
 
 const r1=ReactDOM.createRoot(document.getElementById('root'))
