f907e469a40edb891b4074d315e48ec2307a3c1d
azartechnet <azartechnet@gmail.com>
2024-03-18 09:51:12 +0530

    updated

--- deepamern/src/index.js
+++ deepamern/src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import  ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/*function Sample()
+{
+   return <h1>Welcome to the Application</h1>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const r1=document.getElementById("root");
+const root = ReactDOM.createRoot(r1);
+ root.render(<Sample />)*/
+
+ //Basic Rendering
+
+ /*const App=()=>{
+
+  return (
+    <div>
+      <h1>Welcome  to our application!</h1>
+       <p>This is a paragraph.</p>
+    </div>
+  )
+
+ }
+ const r1=document.getElementById("root");
+const root = ReactDOM.createRoot(r1);
+ root.render(<App />)*/
+
+ //Dynamic Rendering
+
+/*const App=()=>{
+  const username="mohamed";
+  return (
+    <div>
+      <h1>Hello{username}</h1>
+      <p>{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+ }
+ const r1=document.getElementById("root");
+ const root = ReactDOM.createRoot(r1);
+  root.render(<App />)*/
+
+//With JSX
+
+/*const myelem=<h1>Welcome to the JSX</h1>
+const root=ReactDOM.createRoot(document.getElementById('root'));
+root.render(myelem)*/
+
+//Without JSX
+
+const myelem=React.createElement('h1',null,'Welcome to the Js');
+  const root=ReactDOM.createRoot(document.getElementById('root'));
+   root.render(myelem)
+
+   //List
